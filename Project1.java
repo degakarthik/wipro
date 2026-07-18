1b18d85928c04b6d07fa1b31260cf644064ca445
degakarthik <degakarthik07@gmail.com>
2026-07-18 09:37:08 +0530

    Add files via upload

---
+++ Project1.java
@@ -0,0 +1,39 @@
+public class Project1 {
+
+    public static void main(String[] args) {
+
+        int empNo[] = {1001,1002,1003,1004,1005,1006,1007};
+
+        String empName[] = {
+            "Ashish","Sushma","Rahul",
+            "Chahat","Ranjan","Suman","Tanmay"
+        };
+
+        String dept[] = {
+            "R&D","PM","Acct",
+            "Front Desk","Engg",
+            "Manufacturing","PM"
+        };
+
+        char desigCode[] = {
+            'e','c','k','r','m','e','c'
+        };
+
+        int basic[] = {
+            20000,30000,10000,
+            12000,50000,23000,29000
+        };
+
+        int hra[] = {
+            8000,12000,8000,
+            6000,20000,9000,12000
+        };
+
+        int it[] = {
+            3000,9000,1000,
+            2000,20000,4400,10000
+        };
+
+        // Continue with search logic
+    }
+}
